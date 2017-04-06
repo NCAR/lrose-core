0604367d3b97a48d94d67f00a880552e1b916df5
MikeDixon <dixon@ucar.edu>
2017-04-06 15:59:46 -0600

    Adding apps/spdb_utils/WxStn2Table

---
+++ codebase/apps/spdb_utils/src/WxStn2Table/Main.cc
@@ -0,0 +1,112 @@
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* 
+// ** Copyright UCAR (c) 1990 - 2016                                         
+// ** University Corporation for Atmospheric Research (UCAR)                 
+// ** National Center for Atmospheric Research (NCAR)                        
+// ** Boulder, Colorado, USA                                                 
+// ** BSD licence applies - redistribution and use in source and binary      
+// ** forms, with or without modification, are permitted provided that       
+// ** the following conditions are met:                                      
+// ** 1) If the software is modified to produce derivative works,            
+// ** such modified software should be clearly marked, so as not             
+// ** to confuse it with the version available from UCAR.                    
+// ** 2) Redistributions of source code must retain the above copyright      
+// ** notice, this list of conditions and the following disclaimer.          
+// ** 3) Redistributions in binary form must reproduce the above copyright   
+// ** notice, this list of conditions and the following disclaimer in the    
+// ** documentation and/or other materials provided with the distribution.   
+// ** 4) Neither the name of UCAR nor the names of its contributors,         
+// ** if any, may be used to endorse or promote products derived from        
+// ** this software without specific prior written permission.               
+// ** DISCLAIMER: THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS  
+// ** OR IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED      
+// ** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.    
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* 
+///////////////////////////////////////////////////////////////
+//
+// main driver for WxStn2Table
+//
+// Mike Dixon, EOL, NCAR, P.O.Box 3000, Boulder, CO, 80307-3000, USA
+//
+// April 2017
+//
+///////////////////////////////////////////////////////////////
+//
+// WxStn2Table reads surface station entries from an SPDB
+// data base, and based on the specified parameters in the
+// param file, reformats these into a text table.
+//
+////////////////////////////////////////////////////////////////
+
+#include "WxStn2Table.hh"
+#include <csignal>
+#include <new>
+#include <cstdio>
+using namespace std;
+
+// file scope
+
+static void tidy_and_exit(int sig);
+static void out_of_store();
+static WxStn2Table *_prog;
+
+// main
+
+int main(int argc, char **argv)
+  
+{
+
+  // create program object
+
+  _prog = new WxStn2Table(argc, argv);
+  if (!_prog->isOK) {
+    return(-1);
+  }
+
+  // set signal handling
+  
+  signal(SIGINT, tidy_and_exit);
+  signal(SIGHUP, tidy_and_exit);
+  signal(SIGTERM, tidy_and_exit);
+  signal(SIGPIPE, SIG_IGN);
+
+  // set new() memory failure handler function
+
+  set_new_handler(out_of_store);
+
+  // run it
+
+  int iret = _prog->Run();
+
+  // clean up
+
+  tidy_and_exit(iret);
+  return (iret);
+  
+}
+
+///////////////////
+// tidy up on exit
+
+static void tidy_and_exit (int sig)
+
+{
+
+  delete(_prog);
+  exit(sig);
+
+}
+////////////////////////////////////
+// out_of_store()
+//
+// Handle out-of-memory conditions
+//
+
+static void out_of_store()
+
+{
+
+  fprintf(stderr, "FATAL ERROR - program WxStn2Table\n");
+  fprintf(stderr, "  Operator new failed - out of store\n");
+  exit(-1);
+
+}
