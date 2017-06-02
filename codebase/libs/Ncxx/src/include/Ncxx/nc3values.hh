c5c8c213a4abe8c7a2a47f42b2f94715647b8a0e
dixon <dixon@ucar.edu>
2017-06-01 20:45:17 -0600

    adding

--- codebase/libs/Ncxx/src/include/Ncxx/nc3values.hh
+++ codebase/libs/Ncxx/src/include/Ncxx/nc3values.hh
@@ -45,7 +45,7 @@
 #include <iostream>
 #include <sstream>
 #include <limits.h>
-#include "netcdf.h"
+#include <netcdf.h>
 
 // Documentation warned this might change and now it has, for
 // consistency with C interface 
