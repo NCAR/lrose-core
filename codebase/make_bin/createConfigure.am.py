67b915424297a182437cb01e6fdeac1268458834
MikeDixon <dixon@ucar.edu>
2018-08-10 17:03:39 -0600

    make_bin/createConfigure.am.py - fixing test for existence of createScript

--- codebase/make_bin/createConfigure.am.py
+++ codebase/make_bin/createConfigure.am.py
@@ -243,15 +243,13 @@ def searchDir(dir):
         # create makefile.am for app
         # use package version if available
         createScript = "createMakefile.am.app." + options.pkg + ".py"
-        if (options.debug):
-            print >>sys.stderr, "  111111111 createScript:", createScript
         scriptPath = os.path.join(thisScriptDir, createScript)
         if (os.path.exists(scriptPath) == False):
             # no package version, use default
             createScript = "createMakefile.am.app.lrose.py"
             scriptPath = os.path.join(thisScriptDir, createScript)
         if (options.debug):
-            print >>sys.stderr, "  222222222 createScript:", createScript
+            print >>sys.stderr, "  createScript:", createScript
         cmd = scriptPath
         cmd += " --dir " + absDir + debugStr
         cmd += " --libList " + libList
