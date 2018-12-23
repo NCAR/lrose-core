94e689202e02c48bd23c00bb871dc0b7332284e0
MikeDixon <dixon@ucar.edu>
2018-12-22 22:09:44 -0700

    apps/Radx/src/RadxModelQc - changing initialization of FIRFiler to use push_back()

--- codebase/apps/Radx/src/RadxModelQc/FIRFilter.cc
+++ codebase/apps/Radx/src/RadxModelQc/FIRFilter.cc
@@ -10,31 +10,30 @@
 //------------------------------------------------------------------
 FIRFilter::FIRFilter()
 {
-  _coeff = {
-  0.0230,
-  -0.0358,
-  -0.0014,
-  0.0288,
-  -0.0146,
-  -0.0418,
-  0.0356,
-  0.0481,
-  -0.0889,
-  -0.0550,
-  0.3117,
-  0.5561,
-  0.3117,
-  -0.0550,
-  -0.0889,
-  0.0481,
-  0.0356,
-  -0.0418,
-  -0.0146,
-  0.0288,
-  -0.0014,
-  -0.0358,
-  0.0230
-  };
+  // initialize coefficents
+  _coeff.push_back(0.0230);
+  _coeff.push_back(-0.0358);
+  _coeff.push_back(-0.0014);
+  _coeff.push_back(0.0288);
+  _coeff.push_back(-0.0146);
+  _coeff.push_back(-0.0418);
+  _coeff.push_back(0.0356);
+  _coeff.push_back(0.0481);
+  _coeff.push_back(-0.0889);
+  _coeff.push_back(-0.0550);
+  _coeff.push_back(0.3117);
+  _coeff.push_back(0.5561);
+  _coeff.push_back(0.3117);
+  _coeff.push_back(-0.0550);
+  _coeff.push_back(-0.0889);
+  _coeff.push_back(0.0481);
+  _coeff.push_back(0.0356);
+  _coeff.push_back(-0.0418);
+  _coeff.push_back(-0.0146);
+  _coeff.push_back(0.0288);
+  _coeff.push_back(-0.0014);
+  _coeff.push_back(-0.0358);
+  _coeff.push_back(0.0230);
 }
 
 //------------------------------------------------------------------
