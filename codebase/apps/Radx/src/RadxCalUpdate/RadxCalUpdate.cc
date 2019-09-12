1bb87bb6d0c365464a6ec10a3c649dbd62ee5eee
mike-dixon <dixon@ucar.edu>
2019-09-12 21:24:29 +0000

    Adding apps/Radx/src/RadxCalUpdate

---
+++ codebase/apps/Radx/src/RadxCalUpdate/RadxCalUpdate.cc
@@ -0,0 +1,393 @@
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* 
+// ** Copyright UCAR (c) 1990 - 2016                                         
+// ** University Corporation for Atmospheric Research (UCAR)                 
+// ** National Center for Atmospheric Research (NCAR)                        
+// ** Boulder, Colorado, USA                                                 
+// ** BSD licence applies - redistribution and use in source and binary      
+// ** forms, with or without modification, are permitted provided that       
+// ** the following conditions are met:                                      
+// ** 1) If the software is modified to produce derivative works,            
+// ** such modified software should be clearly marked, so as not             
+// ** to confuse it with the version available from UCAR.                    
+// ** 2) Redistributions of source code must retain the above copyright      
+// ** notice, this list of conditions and the following disclaimer.          
+// ** 3) Redistributions in binary form must reproduce the above copyright   
+// ** notice, this list of conditions and the following disclaimer in the    
+// ** documentation and/or other materials provided with the distribution.   
+// ** 4) Neither the name of UCAR nor the names of its contributors,         
+// ** if any, may be used to endorse or promote products derived from        
+// ** this software without specific prior written permission.               
+// ** DISCLAIMER: THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS  
+// ** OR IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED      
+// ** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.    
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* 
+///////////////////////////////////////////////////////////////
+// RadxCalUpdate.cc
+//
+// Mike Dixon, EOL, NCAR, P.O.Box 3000, Boulder, CO, 80307-3000, USA
+//
+// Sept 2019
+//
+///////////////////////////////////////////////////////////////
+//
+// Update the calibration in a Radx file.
+// Also ajusts the DBZ fields accordingly.
+// Optionally corrects the altitude for EGM errors.
+//
+////////////////////////////////////////////////////////////////
+
+#include "RadxCalUpdate.hh"
+#include <Radx/RadxVol.hh>
+#include <Radx/RadxRay.hh>
+#include <Radx/RadxField.hh>
+#include <Mdv/GenericRadxFile.hh>
+#include <Radx/RadxGeoref.hh>
+#include <Radx/RadxTime.hh>
+#include <Radx/RadxTimeList.hh>
+#include <Radx/RadxPath.hh>
+#include <dsserver/DsLdataInfo.hh>
+#include <toolsa/pmu.h>
+using namespace std;
+
+// Constructor
+
+RadxCalUpdate::RadxCalUpdate(int argc, char **argv)
+  
+{
+
+  OK = TRUE;
+
+  // set programe name
+
+  _progName = "RadxCalUpdate";
+  
+  // parse command line args
+  
+  if (_args.parse(argc, argv, _progName)) {
+    cerr << "ERROR: " << _progName << endl;
+    cerr << "Problem with command line args." << endl;
+    OK = FALSE;
+    return;
+  }
+  
+  // get TDRP params
+  
+  _paramsPath = (char *) "unknown";
+  if (_params.loadFromArgs(argc, argv,
+			   _args.override.list, &_paramsPath)) {
+    cerr << "ERROR: " << _progName << endl;
+    cerr << "Problem with TDRP parameters." << endl;
+    OK = FALSE;
+    return;
+  }
+
+}
+
+// destructor
+
+RadxCalUpdate::~RadxCalUpdate()
+
+{
+
+  // unregister process
+
+  PMU_auto_unregister();
+
+}
+
+//////////////////////////////////////////////////
+// Run
+
+int RadxCalUpdate::Run()
+{
+
+  // run
+
+  if (_params.mode == Params::ARCHIVE) {
+    return _runArchive();
+  } else if (_params.mode == Params::FILELIST) {
+    return _runFilelist();
+  } else {
+    return _runRealtime();
+  }
+
+}
+
+//////////////////////////////////////////////////
+// Run in filelist mode
+
+int RadxCalUpdate::_runFilelist()
+{
+
+  // loop through the input file list
+
+  int iret = 0;
+
+  for (int ii = 0; ii < (int) _args.inputFileList.size(); ii++) {
+
+    string inputPath = _args.inputFileList[ii];
+    if (_processFile(inputPath)) {
+      iret = -1;
+    }
+
+  }
+
+  return iret;
+
+}
+
+//////////////////////////////////////////////////
+// Run in archive mode
+
+int RadxCalUpdate::_runArchive()
+{
+
+  // get the files to be processed
+
+  RadxTimeList tlist;
+  tlist.setDir(_params.input_dir);
+  tlist.setModeInterval(_args.startTime, _args.endTime);
+  if (tlist.compile()) {
+    cerr << "ERROR - RadxCalUpdate::_runFilelist()" << endl;
+    cerr << "  Cannot compile time list, dir: " << _params.input_dir << endl;
+    cerr << "  Start time: " << RadxTime::strm(_args.startTime) << endl;
+    cerr << "  End time: " << RadxTime::strm(_args.endTime) << endl;
+    cerr << tlist.getErrStr() << endl;
+    return -1;
+  }
+
+  const vector<string> &paths = tlist.getPathList();
+  if (paths.size() < 1) {
+    cerr << "ERROR - RadxCalUpdate::_runFilelist()" << endl;
+    cerr << "  No files found, dir: " << _params.input_dir << endl;
+    return -1;
+  }
+  
+  // loop through the input file list
+  
+  int iret = 0;
+  for (size_t ii = 0; ii < paths.size(); ii++) {
+    if (_processFile(paths[ii])) {
+      iret = -1;
+    }
+  }
+
+  return iret;
+
+}
+
+//////////////////////////////////////////////////
+// Run in realtime mode
+
+int RadxCalUpdate::_runRealtime()
+{
+
+  // init process mapper registration
+
+  PMU_auto_init(_progName.c_str(), _params.instance,
+                PROCMAP_REGISTER_INTERVAL);
+
+  // watch for new data to arrive
+
+  LdataInfo ldata(_params.input_dir,
+                  _params.debug >= Params::DEBUG_VERBOSE);
+  
+  int iret = 0;
+
+  while (true) {
+    ldata.readBlocking(_params.max_realtime_data_age_secs,
+                       1000, PMU_auto_register);
+    
+    const string path = ldata.getDataPath();
+    if (_processFile(path)) {
+      iret = -1;
+    }
+  }
+
+  return iret;
+
+}
+
+//////////////////////////////////////////////////
+// Process a file
+// Returns 0 on success, -1 on failure
+
+int RadxCalUpdate::_processFile(const string &filePath)
+{
+
+  if (_params.debug) {
+    cerr << "INFO - RadxCalUpdate::Run" << endl;
+    cerr << "  Input path: " << filePath << endl;
+  }
+  
+  RadxFile inFile;
+  _setupRead(inFile);
+  
+  // read in file
+  
+  RadxVol vol;
+  if (inFile.readFromPath(filePath, vol)) {
+    cerr << "ERROR - RadxCalUpdate::Run" << endl;
+    cerr << inFile.getErrStr() << endl;
+    return -1;
+  }
+
+  // fix each ray in turn
+
+  vector<RadxRay *> &rays = vol.getRays();
+  for (size_t iray = 0; iray < rays.size(); iray++) {
+    _fixRay(*rays[iray]);
+  }
+
+  // write the file
+
+  if (_writeVol(vol)) {
+    cerr << "ERROR - RadxCalUpdate::_processFile" << endl;
+    cerr << "  Cannot write volume to file" << endl;
+    return -1;
+  }
+
+  return 0;
+
+}
+
+//////////////////////////////////////////////////
+// set up read
+
+void RadxCalUpdate::_setupRead(RadxFile &file)
+{
+
+  if (_params.debug >= Params::DEBUG_EXTRA) {
+    file.setDebug(true);
+    file.printReadRequest(cerr);
+  }
+
+}
+
+//////////////////////////////////////////////////
+// set up write
+
+void RadxCalUpdate::_setupWrite(RadxFile &file)
+{
+
+  if (_params.debug) {
+    file.setDebug(true);
+  }
+  if (_params.debug >= Params::DEBUG_EXTRA) {
+    file.setVerbose(true);
+  }
+
+  if (_params.output_filename_mode == Params::START_TIME_ONLY) {
+    file.setWriteFileNameMode(RadxFile::FILENAME_WITH_START_TIME_ONLY);
+  } else if (_params.output_filename_mode == Params::END_TIME_ONLY) {
+    file.setWriteFileNameMode(RadxFile::FILENAME_WITH_END_TIME_ONLY);
+  } else {
+    file.setWriteFileNameMode(RadxFile::FILENAME_WITH_START_AND_END_TIMES);
+  }
+
+  if (_params.output_compressed) {
+    file.setWriteCompressed(true);
+    file.setCompressionLevel(_params.compression_level);
+  } else {
+    file.setWriteCompressed(false);
+  }
+
+  // set output format
+
+  file.setFileFormat(RadxFile::FILE_FORMAT_CFRADIAL);
+  file.setNcFormat(RadxFile::NETCDF4);
+
+}
+
+//////////////////////////////////////////////////
+// write out the volume
+
+int RadxCalUpdate::_writeVol(const RadxVol &vol)
+{
+
+  // output file
+
+  GenericRadxFile outFile;
+  _setupWrite(outFile);
+  
+  // write to dir
+  
+  if (outFile.writeToDir(vol, _params.output_dir,
+                         _params.append_day_dir_to_output_dir,
+                         _params.append_year_dir_to_output_dir)) {
+    cerr << "ERROR - FixRadxPointing::_writeVol" << endl;
+    cerr << "  Cannot write file to dir: " << _params.output_dir << endl;
+    cerr << outFile.getErrStr() << endl;
+    return -1;
+  }
+
+  // write latest data info file if requested 
+  
+  if (_params.write_latest_data_info) {
+    string outputPath = outFile.getPathInUse();
+    DsLdataInfo ldata(_params.output_dir);
+    if (_params.debug >= Params::DEBUG_VERBOSE) {
+      ldata.setDebug(true);
+    }
+    string relPath;
+    RadxPath::stripDir(_params.output_dir, outputPath, relPath);
+    ldata.setRelDataPath(relPath);
+    ldata.setWriter(_progName);
+    if (ldata.write(vol.getEndTimeSecs())) {
+      cerr << "WARNING - FixRadxPointing::_writeVol" << endl;
+      cerr << "  Cannot write latest data info file to dir: "
+           << _params.output_dir << endl;
+    }
+  }
+
+  return 0;
+
+}
+
+//////////////////////////////////////////////////
+// fix a ray
+
+void RadxCalUpdate::_fixRay(RadxRay &ray)
+{
+
+  if (_params.update_calibration) {
+    _fixRayCalibration(ray);
+  }
+  
+  if (_params.correct_altitude_for_egm) {
+    _fixRayAltitude(ray);
+  }
+  
+}
+
+//////////////////////////////////////////////////
+// fix a ray's calibration
+
+void RadxCalUpdate::_fixRayCalibration(RadxRay &ray)
+{
+
+  // double fixedDeg = ray.getFixedAngleDeg();
+  // double fixedCorr = fixedDeg + _params.fixed_angle_offset;
+  // if (ray.getSweepMode() == Radx::SWEEP_MODE_RHI) {
+  //   if (fixedCorr < 0) {
+  //     fixedCorr += 360.0;
+  //   } else if (fixedCorr >= 360.0) {
+  //     fixedCorr -= 360.0;
+  //   }
+  // }
+  // ray.setFixedAngleDeg(fixedCorr);
+
+}
+
+//////////////////////////////////////////////////
+// fix a ray's altitude
+
+void RadxCalUpdate::_fixRayAltitude(RadxRay &ray)
+{
+
+  // double elDeg = ray.getElevationDeg();
+  // double elCorr = elDeg + _params.elevation_angle_offset;
+  // ray.setElevationDeg(elCorr);
+
+}
+
