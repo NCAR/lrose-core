3d2cff9a524bc7e7c01810d4d771dcf978cce0fd
mike-dixon <dixon@ucar.edu>
2020-06-07 14:24:29 -0600

    Adding libm.so as exclusion to build/scripts/installOriginLibFiles.py

--- build/scripts/installOriginLibFiles.py
+++ build/scripts/installOriginLibFiles.py
@@ -43,7 +43,7 @@ def main():
                         ',libpthread,libdl,libX,libxpc'
     else:
         # LINUX
-        ignoreDefault = 'libc.so,libpthread.so,libdl.so'
+        ignoreDefault = 'libc.so,libpthread.so,libdl.so,libm.so'
 
     parser = OptionParser(usage)
     parser.add_option('--debug',
