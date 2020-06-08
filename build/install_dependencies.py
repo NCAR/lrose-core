6e018306c19617790155818ba8aa263fc0429425
mike-dixon <dixon@ucar.edu>
2020-06-08 13:11:45 -0600

    build/install_dependencies.py

--- build/install_dependencies.py
+++ build/install_dependencies.py
@@ -6,6 +6,7 @@
 #
 #===========================================================================
 
+from __future__ import print_function
 import os
 import sys
 import shutil
@@ -43,6 +44,9 @@ def main():
                       dest='cidd', default=False,
                       action="store_true",
                       help='Install 32-bit dependencies for CIDD apps')
+    parser.add_option('--osFile',
+                      dest='osFile', default="/etc/os-release",
+                      help='OS file path')
 
     (options, args) = parser.parse_args()
     
@@ -61,134 +65,120 @@ def main():
 
     # let users know what we are doing
 
-    print >>sys.stderr, "****************************************************"
-    print >>sys.stderr, "  Running " + thisScriptName
-    print >>sys.stderr, ""
-    print >>sys.stderr, "  Installing dependencies for LROSE"
-    print >>sys.stderr, "  OS type: " + osType
-    print >>sys.stderr, "  OS version: " + osVersion
-    print >>sys.stderr, ""
-    print >>sys.stderr, "  NCAR, Boulder, CO, USA"
-    print >>sys.stderr, ""
-    print >>sys.stderr, "  " + dateTimeStr
-    print >>sys.stderr, ""
-    print >>sys.stderr, "****************************************************"
+    print("****************************************************", file=sys.stderr)
+    print("Running", thisScriptName, file=sys.stderr)
+    print(" ", dateTimeStr, file=sys.stderr)
+    print("  OS file: ", options.osFile, file=sys.stderr)
+    print("  Installing dependencies for LROSE", file=sys.stderr)
+    print("  OS type: ", osType, file=sys.stderr)
+    print("  OS version: ", osVersion, file=sys.stderr)
+    print("****************************************************", file=sys.stderr)
 
     # do the install
     
     #if (os.path.isdir("bin")):
     #    shellCmd("rsync -av bin " + installDir)
 
-
-    #--------------------------------------------------------------------
     # done
     
-    print("  **************************************************")
-    print("  *** Done installing dependencies ***")
-    print("  *** OS type: " + osType
-    print("  *** OS version: " + osVersion
-    print("  **************************************************")
+    print("****************************************************", file=sys.stderr)
+    print("Done", file=sys.stderr)
+    print("****************************************************", file=sys.stderr)
 
     sys.exit(0)
 
 ########################################################################
-# get the OS type
-
-########################################################################
 # get the OS type from the /etc/os-release file in linux
 
-def getOSType():
+def getOsType():
 
     global osType, osVersion
 
     # Mac OSX?
 
-    if (platform == "darwin"):
-        osType = "OSX"
-        osVersion = 10
-        return
+    # if (platform == "darwin"):
+    #     osType = "OSX"
+    #     osVersion = 10
+    #     return
 
     # not linux?
 
-    if (platform != "linux"):
-        osType = "unknown"
-        osVersion = 0
-        return
+    # if (platform != "linux"):
+    #     osType = "unknown"
+    #     osVersion = 0
+    #     return
 
     # Centos 6?
 
-    if (os.path.isfile("/etc/redhat-release")):
-        rhrelease_file = open("/etc/redhat-release", "rt")
-        lines = rhrelease_file.readlines()
-        rhrelease_file.close()
-        for line in lines:
-          if (line.find("CentOS release 6") == 0):
-          osType = "CentOS"
-          osVersion = 6
-          return
+    # if (os.path.isfile("/etc/redhat-release")):
+    #     rhrelease_file = open("/etc/redhat-release", "rt")
+    #     lines = rhrelease_file.readlines()
+    #     rhrelease_file.close()
+    #     for line in lines:
+    #       if (line.find("CentOS release 6") == 0):
+    #         osType = "centos"
+    #         osVersion = 6
+    #         return
           
-    osrelease_file = open("/etc/os-release", "rt")
-    lines = osrelease_file.readlines()
-    osrelease_file.close()
+    _file = open(options.osFile, "rt")
+    lines = _file.readlines()
+    _file.close()
+
     osType = "unknown"
     for line in lines:
-        if (line.find('PRETTY_NAME') == 0):
+
+        line.strip()
+        
+        if (line.find("NAME") == 0):
+          nameline = line.lower()
+          if (nameline.find("centos") >= 0):
+            osType = "centos"
+          elif (nameline.find("fedora") >= 0):
+            osType = "fedora"
+          elif (nameline.find("debian") >= 0):
+            osType = "debian"
+          elif (nameline.find("ubuntu") >= 0):
+            osType = "ubuntu"
+          elif (nameline.find("suse") >= 0):
+            osType = "suse"
+            
+        if (line.find("VERSION_ID") == 0):
           lineParts = line.split('=')
-          osParts = lineParts[1].split('"')
-          osType = osParts[1]
+          print("  lineParts: ", lineParts, file=sys.stderr)
+          if (lineParts[1].find('"') >= 0):
+            subParts = lineParts[1].split('"')
+            print("  subParts: ", subParts, file=sys.stderr)
+            osVersion = float(subParts[1])
+          else:
+            osVersion = float(lineParts[1])
+
           return
 
-def getOsType():
-
-
-
-    ostype = "x86_64"
-    tmpFile = os.path.join("/tmp", "ostype." + timeStr + ".txt")
-
-    shellCmd("uname -a > " + tmpFile)
-    f = open(tmpFile, 'r')
-    lines = f.readlines()
-    f.close()
-
-    if (len(lines) < 1):
-        print >>sys.stderr, "ERROR getting OS type"
-        print >>sys.stderr, "  'uname -a' call did not succeed"
-        sys.exit(1)
-
-    for line in lines:
-        line = line.strip()
-        if (options.verbose):
-            print >>sys.stderr, "  line: ", line
-        if (line.find("x86_64") > 0):
-            ostype = "x86_64"
-        elif (line.find("i686") > 0):
-            ostype = "i686"
-            
 ########################################################################
 # Run a command in a shell, wait for it to complete
 
 def shellCmd(cmd):
 
     if (options.debug):
-        print >>sys.stderr, "running cmd:", cmd, " ....."
+        print("running cmd:", cmd, " .....", file=sys.stderr)
     
     try:
         retcode = subprocess.check_call(cmd, shell=True)
         if retcode != 0:
-            print >>sys.stderr, "Child exited with code: ", retcode
+            print("Child exited with code: ", retcode, file=sys.stderr)
             sys.exit(1)
         else:
             if (options.verbose):
-                print >>sys.stderr, "Child returned code: ", retcode
-    except OSError, e:
-        print >>sys.stderr, "Execution failed:", e
+                print("Child returned code: ", retcode, file=sys.stderr)
+    except OSError as e:
+        print("Execution failed:", e, file=sys.stderr)
         sys.exit(1)
 
     if (options.debug):
-        print >>sys.stderr, ".... done"
+        print(".... done", file=sys.stderr)
     
 ########################################################################
 # Run - entry point
 
 if __name__ == "__main__":
-   main()
+    main()
