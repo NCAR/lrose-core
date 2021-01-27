c9c32a3193134f3cbdabf52a70cdcda2da02c65a
mike-dixon <dixon@ucar.edu>
2021-01-26 21:51:43 -0700

    Adding find_package netcdf

--- build/cmake/createCMakeLists.py
+++ build/cmake/createCMakeLists.py
@@ -516,7 +516,8 @@ def writeCMakeListsTop(dir):
     fo.write('endif(APPLE)\n')
 
     fo.write('find_package (Qt5 COMPONENTS Widgets Network Qml REQUIRED PATHS /usr NO_DEFAULT_PATH)\n')
-    fo.write('find_package (HDF5 COMPONENTS C CXX)\n')
+    fo.write('find_package (HDF5 COMPONENTS C CXX REQUIRED)\n')
+    fo.write('find_package (NETCDF REQUIRED)\n')
     fo.write('\n')
 
     if (len(options.installDir) == 0):
