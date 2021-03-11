cdc772d65ebd74c622c32196eb2b51e29d163515
mike-dixon <dixon@ucar.edu>
2021-03-11 15:41:26 -0700

    build/cmake/createCMakeLists.py - working on qt5 for osx

--- build/cmake/createCMakeLists.py
+++ build/cmake/createCMakeLists.py
@@ -523,7 +523,7 @@ def writeCMakeListsTop(dir):
     fo.write('SET(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/../build/cmake/"\n')
     fo.write('     CACHE INTERNAL "Location of our custom CMake modules.")\n')
     fo.write('\n')
-
+ 
     fo.write('SET(CMAKE_PREFIX_PATH "${CMAKE_CURRENT_SOURCE_DIR}/build/cmake")\n')
     fo.write('\n')
 
@@ -540,7 +540,7 @@ def writeCMakeListsTop(dir):
     fo.write('# Finding Qt on mac OSX\n')
     fo.write('\n')
     fo.write('if(APPLE)\n')
-    fo.write('find_path(Qt5_DIR NAMES Qt5Config.cmake qt5-config.cmake HINTS /usr/local/Cellar/qt*/*/lib/cmake/Qt5 NO_DEFAULT_PATH)\n')
+    fo.write('find_path(Qt5_DIR NAMES Qt5Config.cmake qt5-config.cmake HINTS /usr/local/Cellar/qt@5/*/lib/cmake/Qt5 NO_DEFAULT_PATH)\n')
     fo.write('endif(APPLE)\n')
 
     # fo.write('find_package (Qt5 COMPONENTS Widgets Network Qml REQUIRED PATHS /usr NO_DEFAULT_PATH)\n')
