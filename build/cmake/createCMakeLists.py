44d1143063bc5f63a7b3d5a3192bc17588bcaf21
Jian Sun <sjsprecious@gmail.com>
2021-07-01 14:58:53 -0600

    Add -fPIC option to C compiler

--- build/cmake/createCMakeLists.py
+++ build/cmake/createCMakeLists.py
@@ -605,6 +605,8 @@ def writeCMakeListsTop(dir):
 
     if (options.m32):
         fo.write('set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC -m32 ")\n')
+    else:
+        fo.write('set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC ")\n')
 
     fo.write('set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -std=c++11 ")\n')
     if (options.m32):
