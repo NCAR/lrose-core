494015c4ccbe30e2db42f43d2b01ec55588ec05a
Jian Sun <sjsprecious@gmail.com>
2021-07-07 16:07:01 -0600

    add __fujitsu__ flag

--- build/cmake/createCMakeLists.py
+++ build/cmake/createCMakeLists.py
@@ -622,7 +622,7 @@ def writeCMakeListsTop(dir):
     if (options.iscray):
         fo.write('set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -hstd=c++11 ")\n')
     elif (options.isfujitsu):
-        fo.write('set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -std=c++14 ")\n')        
+        fo.write('set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -std=c++11 -D__fujitsu__ ")\n')        
     else:
         fo.write('set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -std=c++11 ")\n')
         
