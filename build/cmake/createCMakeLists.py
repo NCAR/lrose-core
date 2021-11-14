a459655e4f2422e9c2f231601060dc9bf323f6c3
mike-dixon <dixon@ucar.edu>
2021-11-13 17:40:38 -0700

    build/cmake/createCMakeLists.py - adding find_package(LROSE) at the top level

--- build/cmake/createCMakeLists.py
+++ build/cmake/createCMakeLists.py
@@ -559,6 +559,7 @@ def writeCMakeListsTop(dir):
     fo.write("endif()\n")
     fo.write('find_package (HDF5)\n')
     fo.write('find_package (NETCDF)\n')
+    fo.write('find_package (LROSE)\n')
     fo.write("if (DEFINED HDF5_hdf5_LIBRARY_RELEASE)\n")
     fo.write("  get_filename_component(HDF5_INSTALL_PREFIX ${HDF5_hdf5_LIBRARY_RELEASE} DIRECTORY)\n")
     fo.write("endif()\n")
