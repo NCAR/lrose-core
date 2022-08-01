52ea7dbfc1f5290edae3e168932b2fef02ca6276
mike-dixon <dixon@ucar.edu>
2022-08-01 06:03:02 +0000

    Adding apps/Radx/src/RadxStats

---
+++ codebase/apps/Radx/src/RadxStats/Args.cc
@@ -0,0 +1,222 @@
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* 
+// ** Copyright UCAR (c) 1990 - 2016                                         
+// ** University Corporation for Atmospheric Research (UCAR)                 
+// ** National Center for Atmospheric Research (NCAR)                        
+// ** Boulder, Colorado, USA                                                 
+// ** BSD licence applies - redistribution and use in source and binary      
+// ** forms, with or without modification, are permitted provided that       
+// ** the following conditions are met:                                      
+// ** 1) If the software is modified to produce derivative works,            
+// ** such modified software should be clearly marked, so as not             
+// ** to confuse it with the version available from UCAR.                    
+// ** 2) Redistributions of source code must retain the above copyright      
+// ** notice, this list of conditions and the following disclaimer.          
+// ** 3) Redistributions in binary form must reproduce the above copyright   
+// ** notice, this list of conditions and the following disclaimer in the    
+// ** documentation and/or other materials provided with the distribution.   
+// ** 4) Neither the name of UCAR nor the names of its contributors,         
+// ** if any, may be used to endorse or promote products derived from        
+// ** this software without specific prior written permission.               
+// ** DISCLAIMER: THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS  
+// ** OR IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED      
+// ** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.    
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* 
+//////////////////////////////////////////////////////////
+// Args.cc : command line args
+//
+// Mike Dixon, RAP, NCAR, P.O.Box 3000, Boulder, CO, 80307-3000, USA
+//
+// June 2012
+//
+//////////////////////////////////////////////////////////
+
+#include "Args.hh"
+#include "Params.hh"
+#include <string>
+#include <iostream>
+#include <Radx/RadxTime.hh>
+using namespace std;
+
+// Constructor
+
+Args::Args ()
+{
+  TDRP_init_override(&override);
+  startTime = 0;
+  endTime = 0;
+}
+
+// Destructor
+
+Args::~Args ()
+
+{
+  TDRP_free_override(&override);
+}
+
+// parse the command line
+//
+// returns 0 on success, -1 on failure  
+
+int Args::parse (int argc, char **argv, string &prog_name)
+
+{
+
+  _progName = prog_name;
+
+  char tmp_str[BUFSIZ];
+  bool OK = true;
+  vector<string> fields;
+  
+  // loop through args
+  
+  for (int i =  1; i < argc; i++) {
+    
+    if (!strcmp(argv[i], "--") ||
+	!strcmp(argv[i], "-h") ||
+	!strcmp(argv[i], "-help") ||
+	!strcmp(argv[i], "-man")) {
+      
+      _usage(cout);
+      exit (0);
+      
+    } else if (!strcmp(argv[i], "-d") ||
+               !strcmp(argv[i], "-debug")) {
+      
+      sprintf(tmp_str, "debug = DEBUG_NORM;");
+      TDRP_add_override(&override, tmp_str);
+      
+    } else if (!strcmp(argv[i], "-v") ||
+               !strcmp(argv[i], "-verbose")) {
+      
+      sprintf(tmp_str, "debug = DEBUG_VERBOSE;");
+      TDRP_add_override(&override, tmp_str);
+      
+    } else if (!strcmp(argv[i], "-vv") ||
+               !strcmp(argv[i], "-extra")) {
+      
+      sprintf(tmp_str, "debug = DEBUG_EXTRA;");
+      TDRP_add_override(&override, tmp_str);
+      
+    } else if (!strcmp(argv[i], "-check_ngates_constant")) {
+      
+      sprintf(tmp_str, "check_ngates_constant = TRUE;");
+      TDRP_add_override(&override, tmp_str);
+      
+    } else if (!strcmp(argv[i], "-check_ngates_vary")) {
+      
+      sprintf(tmp_str, "check_ngates_vary = TRUE;");
+      TDRP_add_override(&override, tmp_str);
+      
+    } else if (!strcmp(argv[i], "-start")) {
+      
+      if (i < argc - 1) {
+	startTime = RadxTime::parseDateTime(argv[++i]);
+	if (startTime == RadxTime::NEVER) {
+	  OK = false;
+	} else {
+	  sprintf(tmp_str, "mode = ARCHIVE;");
+	  TDRP_add_override(&override, tmp_str);
+	}
+      } else {
+	OK = false;
+      }
+	
+    } else if (!strcmp(argv[i], "-end")) {
+      
+      if (i < argc - 1) {
+	endTime = RadxTime::parseDateTime(argv[++i]);
+	if (endTime == RadxTime::NEVER) {
+	  OK = false;
+	} else {
+	  sprintf(tmp_str, "mode = ARCHIVE;");
+	  TDRP_add_override(&override, tmp_str);
+	}
+      } else {
+	OK = false;
+      }
+	
+    } else if (!strcmp(argv[i], "-f")) {
+      
+      if (i < argc - 1) {
+	// load up file list vector. Break at next arg which
+	// start with -
+	for (int j = i + 1; j < argc; j++) {
+	  if (argv[j][0] == '-') {
+	    break;
+	  } else {
+	    inputFileList.push_back(argv[j]);
+	  }
+	}
+	sprintf(tmp_str, "mode = FILELIST;");
+	TDRP_add_override(&override, tmp_str);
+      } else {
+	OK = false;
+      }
+      
+    } else if (!strcmp(argv[i], "-indir")) {
+      
+      if (i < argc - 1) {
+	sprintf(tmp_str, "input_dir = \"%s\";", argv[++i]);
+	TDRP_add_override(&override, tmp_str);
+      } else {
+	OK = false;
+      }
+	
+    }
+    
+  } // i
+
+  if (!OK) {
+    _usage(cerr);
+    return -1;
+  }
+
+  return 0;
+    
+}
+
+void Args::_usage(ostream &out)
+{
+
+  out << "Usage: " << _progName << " [args as below]\n"
+      << "Options:\n"
+      << "\n"
+      << "  [ -h ] produce this list.\n"
+      << "\n"
+      << "  [ -ag ] aggregate sweep files into volume on read.\n"
+      << "     Files with the SAME VOLUME NUMBER in the name are aggregated.\n"
+      << "     Applies to CfRadial and DORADE sweep files.\n"
+      << "\n"
+      << "  [ -check_ngates_constant ] check that ngates are constant\n"
+      << "     Print message if ngates are NOT constant in volume\n"
+      << "\n"
+      << "  [ -check_ngates_vary ] check that ngates vary in the volume\n"
+      << "     Print message if ngates do NOT vary in volume\n"
+      << "\n"
+      << "  [ -d, -debug ] print debug messages\n"
+      << "\n"
+      << "  [ -end \"yyyy mm dd hh mm ss\"] end time\n"
+      << "     Sets mode to ARCHIVE\n"
+      << "\n"
+      << "  [ -f, -paths ? ] set file paths\n"
+      << "     Sets mode to FILELIST\n"
+      << "\n"
+      << "  [ -indir ? ] set input directory\n"
+      << "\n"
+      << "  [ -start \"yyyy mm dd hh mm ss\"] start time\n"
+      << "     Sets mode to ARCHIVE\n"
+      << "\n"
+      << "  [ -v, -verbose ] print verbose debug messages\n"
+      << "\n"
+      << "  [ -vv, -extra ] print extra verbose debug messages\n"
+      << "\n"
+      << endl;
+  
+  out << "NOTE: You do not need to use the params option (see below).\n"
+      << "      If no params are specified, you deal with the whole file.\n"
+      << endl;
+
+  Params::usage(out);
+  
+}
---
+++ codebase/apps/Radx/src/RadxStats/RadxStats.cc
@@ -0,0 +1,290 @@
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* 
+// ** Copyright UCAR (c) 1990 - 2016                                         
+// ** University Corporation for Atmospheric Research (UCAR)                 
+// ** National Center for Atmospheric Research (NCAR)                        
+// ** Boulder, Colorado, USA                                                 
+// ** BSD licence applies - redistribution and use in source and binary      
+// ** forms, with or without modification, are permitted provided that       
+// ** the following conditions are met:                                      
+// ** 1) If the software is modified to produce derivative works,            
+// ** such modified software should be clearly marked, so as not             
+// ** to confuse it with the version available from UCAR.                    
+// ** 2) Redistributions of source code must retain the above copyright      
+// ** notice, this list of conditions and the following disclaimer.          
+// ** 3) Redistributions in binary form must reproduce the above copyright   
+// ** notice, this list of conditions and the following disclaimer in the    
+// ** documentation and/or other materials provided with the distribution.   
+// ** 4) Neither the name of UCAR nor the names of its contributors,         
+// ** if any, may be used to endorse or promote products derived from        
+// ** this software without specific prior written permission.               
+// ** DISCLAIMER: THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS  
+// ** OR IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED      
+// ** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.    
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* 
+///////////////////////////////////////////////////////////////
+// RadxStats.cc
+//
+// Mike Dixon, EOL, NCAR, P.O.Box 3000, Boulder, CO, 80307-3000, USA
+//
+// Aug 2022
+//
+///////////////////////////////////////////////////////////////
+//
+// RadxStats compute stats on CfRadial files.
+//
+///////////////////////////////////////////////////////////////
+
+#include "RadxStats.hh"
+#include <Radx/RadxVol.hh>
+#include <Radx/RadxRay.hh>
+#include <Radx/RadxField.hh>
+#include <Radx/NcfRadxFile.hh>
+#include <Radx/DoradeRadxFile.hh>
+#include <Radx/ForayNcRadxFile.hh>
+#include <Radx/NexradRadxFile.hh>
+#include <Radx/UfRadxFile.hh>
+#include <Radx/RadxGeoref.hh>
+#include <Radx/RadxTime.hh>
+#include <Radx/RadxTimeList.hh>
+#include <Radx/RadxPath.hh>
+#include <didss/LdataInfo.hh>
+#include <toolsa/pmu.h>
+using namespace std;
+
+// Constructor
+
+RadxStats::RadxStats(int argc, char **argv)
+  
+{
+
+  OK = TRUE;
+  
+  // set programe name
+
+  _progName = "RadxStats";
+  
+  // parse command line args
+  
+  if (_args.parse(argc, argv, _progName)) {
+    cerr << "ERROR: " << _progName << endl;
+    cerr << "Problem with command line args." << endl;
+    OK = FALSE;
+    return;
+  }
+  
+  // get TDRP params
+  
+  _paramsPath = (char *) "unknown";
+  if (_params.loadFromArgs(argc, argv,
+			   _args.override.list, &_paramsPath)) {
+    cerr << "ERROR: " << _progName << endl;
+    cerr << "Problem with TDRP parameters." << endl;
+    OK = FALSE;
+    return;
+  }
+
+}
+
+// destructor
+
+RadxStats::~RadxStats()
+
+{
+
+  // unregister process
+
+  PMU_auto_unregister();
+
+}
+
+//////////////////////////////////////////////////
+// Run
+
+int RadxStats::Run()
+{
+
+  if (_params.mode == Params::ARCHIVE) {
+    return _runArchive();
+  } else if (_params.mode == Params::FILELIST) {
+    return _runFilelist();
+  } else {
+    return _runRealtime();
+  }
+}
+
+//////////////////////////////////////////////////
+// Run in filelist mode
+
+int RadxStats::_runFilelist()
+{
+
+  // loop through the input file list
+
+  int iret = 0;
+
+  for (int ii = 0; ii < (int) _args.inputFileList.size(); ii++) {
+
+    string inputPath = _args.inputFileList[ii];
+    if (_processFile(inputPath)) {
+      iret = -1;
+    }
+
+  }
+
+  return iret;
+
+}
+
+//////////////////////////////////////////////////
+// Run in archive mode
+
+int RadxStats::_runArchive()
+{
+
+  // get the files to be processed
+
+  RadxTimeList tlist;
+  tlist.setDir(_params.input_dir);
+  tlist.setModeInterval(_args.startTime, _args.endTime);
+  if (tlist.compile()) {
+    cerr << "ERROR - RadxStats::_runFilelist()" << endl;
+    cerr << "  Cannot compile time list, dir: " << _params.input_dir << endl;
+    cerr << "  Start time: " << RadxTime::strm(_args.startTime) << endl;
+    cerr << "  End time: " << RadxTime::strm(_args.endTime) << endl;
+    cerr << tlist.getErrStr() << endl;
+    return -1;
+  }
+
+  const vector<string> &paths = tlist.getPathList();
+  if (paths.size() < 1) {
+    cerr << "ERROR - RadxStats::_runFilelist()" << endl;
+    cerr << "  No files found, dir: " << _params.input_dir << endl;
+    return -1;
+  }
+  
+  // loop through the input file list
+  
+  int iret = 0;
+  for (size_t ii = 0; ii < paths.size(); ii++) {
+    if (_processFile(paths[ii])) {
+      iret = -1;
+    }
+  }
+
+  return iret;
+
+}
+
+//////////////////////////////////////////////////
+// Run in realtime mode
+
+int RadxStats::_runRealtime()
+{
+
+  // init process mapper registration
+
+  PMU_auto_init(_progName.c_str(), _params.instance,
+                PROCMAP_REGISTER_INTERVAL);
+
+  // watch for new data to arrive
+
+  LdataInfo ldata(_params.input_dir,
+                  _params.debug >= Params::DEBUG_VERBOSE);
+  
+  int iret = 0;
+
+  while (true) {
+    ldata.readBlocking(_params.max_realtime_data_age_secs,
+                       1000, PMU_auto_register);
+    
+    const string path = ldata.getDataPath();
+    if (_processFile(path)) {
+      iret = -1;
+    }
+  }
+
+  return iret;
+
+}
+
+//////////////////////////////////////////////////
+// Process a file
+// Returns 0 on success, -1 on failure
+
+int RadxStats::_processFile(const string &filePath)
+{
+
+  // check we have not already processed this file
+  // in the file aggregation step
+
+  RadxPath thisPath(filePath);
+  for (size_t ii = 0; ii < _readPaths.size(); ii++) {
+    RadxPath rpath(_readPaths[ii]);
+    if (thisPath.getFile() == rpath.getFile()) {
+      if (_params.debug >= Params::DEBUG_VERBOSE) {
+        cerr << "Skipping file: " << filePath << endl;
+        cerr << "  Previously processed in aggregation step" << endl;
+      }
+      return 0;
+    }
+  }
+  
+  if (_params.debug) {
+    cerr << "INFO - RadxStats::Run" << endl;
+    cerr << "  Input path: " << filePath << endl;
+  }
+  
+  RadxFile inFile;
+  _setupRead(inFile);
+  
+  // read in file
+  
+  RadxVol vol;
+  if (inFile.readFromPath(filePath, vol)) {
+    cerr << "ERROR - RadxStats::Run" << endl;
+    cerr << inFile.getErrStr() << endl;
+    return -1;
+  }
+  _readPaths = inFile.getReadPaths();
+
+  // perform the checks
+
+  if (_params.check_ngates_constant) {
+    if (vol.getNGatesVary()) {
+      cout << "N gates not constant, file: " << filePath << endl;
+    }
+  }
+
+  if (_params.check_ngates_vary) {
+    if (!vol.getNGatesVary()) {
+      cout << "N gates do not vary, file: " << filePath << endl;
+    }
+  }
+
+  return 0;
+
+}
+
+//////////////////////////////////////////////////
+// set up read
+
+void RadxStats::_setupRead(RadxFile &file)
+{
+
+  if (_params.debug >= Params::DEBUG_VERBOSE) {
+    file.setDebug(true);
+  }
+  if (_params.debug >= Params::DEBUG_EXTRA) {
+    file.setVerbose(true);
+  }
+
+  file.setReadAggregateSweeps(false);
+
+  file.setReadMetadataOnly(true);
+    
+  if (_params.debug >= Params::DEBUG_EXTRA) {
+    file.printReadRequest(cerr);
+  }
+  
+}
+
